f6658705b212605770838cdc0e1c0a8005f1c632
Nick Herlihy <nherlihy@sessionm.com>
2016-06-24 11:35:46 -0400

    Handling EmptyStackException

--- src/edu/wit/comp2071/group3/stackapplication2/CalculatorOG.java
+++ src/edu/wit/comp2071/group3/stackapplication2/CalculatorOG.java
@@ -87,17 +87,26 @@ public class CalculatorOG {
 	                Operands.push(v2);
 	                tempOperators.push(optr);
 	            }                
-	        }    
+	        }
 	        /* Push back all elements from temporary stacks to main stacks */            
-	        while (!tempOperands.isEmpty())
+	        while (!tempOperands.isEmpty()){
 	            Operands.push(tempOperands.pop());
-	        while (!tempOperators.isEmpty())
+	        }
+	        while (!tempOperators.isEmpty()){
 	            Operators.push(tempOperators.pop());
+	        }
 	        /* Iterate again for same operator */
-	        if (it)
-	            i--;                            
-	    }    
-	    System.out.println("\nResult = "+Operands.pop());        
+	        if (it){
+	            i--;
+	        }
+	    }
+	    try{
+	    	System.out.println("\nResult = "+Operands.pop());
+	    }
+	    catch(java.util.EmptyStackException e){
+	    	//If division by 0 then the stack is left with zero entries and no result
+	    	//Will catch and just continue
+	    }
 	}	
 
 	public static void main(String[] args){
